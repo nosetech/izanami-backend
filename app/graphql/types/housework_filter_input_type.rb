8867715baa960a48a86d1944ad49f781dc9a88d5
Hiroyuki Nose <80944036+nosetech@users.noreply.github.com>
2025-10-05 16:57:02 +0900

    Add category filter to houseworks GraphQL query (#24)

--- app/graphql/types/housework_filter_input_type.rb
+++ app/graphql/types/housework_filter_input_type.rb
@@ -4,5 +4,6 @@ module Types
     argument :suggested_by_id, ID, required: false, description: "Filter by suggester user ID"
     argument :point_min, Integer, required: false, description: "Minimum point value"
     argument :point_max, Integer, required: false, description: "Maximum point value"
+    argument :categories, [ Types::HouseworkCategoryEnum ], required: false, description: "Filter by categories (OR condition)"
   end
 end
